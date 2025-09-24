ff034f76bb426249a7990ce5385e5412458e2421
Jeff Ouda <jeffoudahabakuk@gmail.com>
2025-09-25 00:12:28 +0300

    test complete

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+  const button = document.getElementById("button"); // get the input by its id
+  button.addEventListener("click", () => {
+    alert("Button was clicked!"); // or console.log("clicked")
+  });
 }
